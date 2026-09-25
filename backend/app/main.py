3fdc56936652d5155cead292f7cacfb97971d249
Hive <cianwknight@gmail.com>
2026-09-25 16:44:40 -0400

    mistyped import haha

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from .db.database import Base, engine
 from .db import models
 
-from .routers import conversations, intents, routers, speech
+from .routers import conversations, intents, responses, speech
 
 app = FastAPI(title="AAC API")
 
